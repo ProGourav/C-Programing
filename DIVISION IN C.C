c4f5cf0203112ce216e6259e08bfe92c3042d365
ProGourav <gouravsingla026@gmail.com>
2026-08-31 22:09:23 +0530

    Add division program in C
    Implement basic division functionality in C.

---
+++ DIVISION IN C.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float a, b, division;
+clrscr();
+printf("Enter the Value of a: ");
+scanf("%f", &a);
+printf("Enter the Value of b: ");
+scanf("%f", &b);
+division = a / b;
+printf("The division of a * b = %f", division);
+getch();
+}
