dc4d3db5088642d866c9a6dbbeee6fb85da7418c
ProGourav <gouravsingla026@gmail.com>
2026-08-31 22:08:51 +0530

    Update and rename 3.C to MULTIPLICATION IN C.C

---
+++ MULTIPLICATION IN C.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float a, b, multiplication;
+clrscr();
+printf("Enter the Value of a: ");
+scanf("%f", &a);
+printf("Enter the Value of b: ");
+scanf("%f", &b);
+multiplication = a * b;
+printf("The multiplication of a * b = %f", multiplication);
+getch();
+}
